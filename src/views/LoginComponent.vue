1aae70ba916e9c2b5ac3322e3a43e9d7ecf426b5
GreatJang <1wkdwnsgur@gmail.com>
2024-02-19 18:21:17 +0900

    Login_update

--- src/views/LoginComponent.vue
+++ src/views/LoginComponent.vue
@@ -33,8 +33,8 @@ export default {
         }
     },
     methods:{
-      async doLogin(){
-
+      async doLogin(){                      
+        
         // 1. 200번대 상태값 + 토큰x 
         // 2. 200번대 상태값x -> 분기처리
       try{
